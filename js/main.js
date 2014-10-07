6d9e02825cff0c3197aa60dddae996351b61d3ae
therobreynolds <rreynolds@qlarion.com>
2014-10-07 11:38:13 -0400

    upload csv files

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
-	var permitNumber = "";
 	$('#submitBtn').click(function() {
-		//console.log(permitNumber.substring(0,3).toLowerCase());
-		permitNumber = $('#permitTxt').val();
+		var permitNumber = "";
+		permitNumber = $('#permitTxt').val().toString();
 		if(permitNumber.substring(0,3).toLowerCase()==="bfd"){
-			alert("I am an alert box!");
 			window.location.href = "details_fire.html?id="+$('#permitTxt').val();
 		}
 		else {
-			alert("I am not an alert box!");
 			window.location.href = "details.html?id="+$('#permitTxt').val();
-		}
-	    
+		}   
 	});
-
 	$("#permitTxt").keyup(function(event){
 	    if(event.keyCode == 13){
 	        $('#submitBtn').click();
